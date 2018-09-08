818e11de74f094efedcd444f62ce0c25f72ac0bc
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-09-08 05:24:22 +0300

    LWDEV-8684 Logging in client improved a bit.

--- client/Lykke.Service.MicrographCache.Client/AutofacExtension.cs
+++ client/Lykke.Service.MicrographCache.Client/AutofacExtension.cs
@@ -21,8 +21,6 @@ namespace Lykke.Service.MicrographCache.Client
 
         public static void RegisterMicrographCacheClient(this ContainerBuilder builder, string serviceUrl)
         {
-            if (builder == null) throw new ArgumentNullException(nameof(builder));
-            if (serviceUrl == null) throw new ArgumentNullException(nameof(serviceUrl));
             if (string.IsNullOrWhiteSpace(serviceUrl))
                 throw new ArgumentException("Value cannot be null or whitespace.", nameof(serviceUrl));
 
